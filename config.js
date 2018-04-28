e2ad8e97ef021b111d0c368259c59ec7a0fcae10
luke <azathothbeats@gmail.com>
2018-04-27 22:00:18 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'ByHdaR05s9322IJHkOQlogeWG',
+  consumer_secret: 'PtAJbTG9y40m3wuHPcxDiBWvZGYt3fZCQN06M1K9IMU1poA5dO',
+  access_token: '237631149-u6y645wwxvyehEpztzGhPMa8L2f4iQ5Gul4ucUVh',
+  access_token_secret: 'mMfSovRYXhVKm57T3qfLUHNRyxdORG7udsBkJsuDTBZgy'
+}
